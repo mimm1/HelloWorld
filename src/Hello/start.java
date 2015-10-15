aa56b2029ce79ec8c134b7255db770e4e4aac540
mimm1 <mmadrees@uod.edu.sa>
2015-10-15 12:48:44 +0300

    First Commit
    This is my First Commit to Hello World

---
+++ src/Hello/start.java
@@ -0,0 +1,19 @@
+package Hello;
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 29;
+		 
+		if (age < 19)
+		{
+		  System.out.println("You are not an adult.");
+		}
+		else
+		{
+		  System.out.println("You are an adult.");
+		}
+	}
+
+}
